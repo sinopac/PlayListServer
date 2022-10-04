f9616b82d55373f16bf3077cdd8c8bf05f3c2d31
sinopac <paojen.huang@gmail.com>
2022-10-03 23:43:46 -0400

    Add Pydantic password validator

--- schemas/users.py
+++ schemas/users.py
@@ -3,6 +3,14 @@ from pydantic import BaseModel, EmailStr, validator
 
 class UserCreate(BaseModel):
     last_name: str
+    middle_name: Optional[str]
     first_name: str
     email: EmailStr
     password: str
+    password1: str
+
+    @validator('password1')
+    def passwords_match(cls, v, values, **kwargs):
+        if 'password' in values and v != values['password']:
+            raise ValueError('passwords do not match')
+        return v
